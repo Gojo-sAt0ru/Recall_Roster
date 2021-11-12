2b6874abee7d0543aefe5e030574237364a6ea46
Gojo-sAt0ru <92492736+Gojo-sAt0ru@users.noreply.github.com>
2021-11-12 17:53:42 -0600

    Update Recall_Roster.py

--- Recall_Roster.py
+++ Recall_Roster.py
@@ -2,8 +2,8 @@ import tkinter as tk
 app = tk.Tk()
 
 Recall_Elements = {
-"FName": "Fake First Name",
-"LName": "Fake Last Name",
+"First Name": "Larry",
+"Last Name": "Fake Last Name",
 "Address": "Fake Address",
 "Phone #": "Fake Phone",
 "Email": "Fake Email"
@@ -12,21 +12,21 @@ Recall_Elements = {
 def get_input():
     global entry
     string= entry.get()
-    if string in Recall_Elements["FName"]:
-        label2 = tk.Label(text= Recall_Elements)
+    if string in Recall_Elements["First Name"]:
+        label2 = tk.Label(text= Recall_Elements["Address"])
         label2.pack()
          
-label = tk.Label(text="Who Are You Looking For",
-    foreground="white",
-    background='#34A2FE',
-    width=30,
-    height=10)
+label = tk.Label(text="Who Are You Looking For")
+label2 = tk.Label(text= "What are you looking for")
 entry = tk.Entry()
+entry2 = tk.Entry()
 button = tk.Button(text= "Search", width= 20, command=get_input)
 
 
 label.pack()
 entry.pack()
+label2.pack()
+entry2.pack()
 button.pack()
 
 
