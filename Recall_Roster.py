35eaeb9120b302673b4e714ca8b27e6ff352bdb2
Gojo-sAt0ru <92492736+Gojo-sAt0ru@users.noreply.github.com>
2021-11-19 09:50:28 -0600

    Update Recall_Roster.py

--- Recall_Roster.py
+++ Recall_Roster.py
@@ -14,9 +14,6 @@ recall_roster= {
 "Phone #": "Fake Phone",
 "Email": "Fake Email"}
     
-
-
-         
 label = tk.Label(text="Who Are You Looking For")
 label2 = tk.Label(text= "What are you looking for")
 entry = tk.Entry()
@@ -24,25 +21,26 @@ entry2 = tk.Entry()
 button = tk.Button(text= "Search", width= 20, command= lambda: [get_input(), get_2nd_input(), print_it()])
 final = tk.Label()
 
-def get_input():
+def get_input(): # grabs name input from UI
     global entry
     global string
     string= entry.get()
     
-
-   
-def get_2nd_input():
+def get_2nd_input(): # grabs what input element of the dictionary 
     global entry2
     global string2
     string2= entry2.get()
 
-   
-
-def print_it():
+def print_it(): # Eventually will print result to screen
     if string in str(recall_roster):
-        final = tk.Label(text= )
+        final = tk.Label(text= recall_roster)
         final.pack()
 
+def converter(string, string2): # Converting the string input to the index so that it can be returned 
+    for i in recall_roster:
+        for j in i:
+            if str(j) == str(string):
+                return 
 
 label.pack()
 entry.pack()
@@ -52,3 +50,4 @@ button.pack()
 
 
 app.mainloop()
+
